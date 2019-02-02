d382a149d1f6686823b342a42c967870a0468cf3
Mikhail Firulin <mfirulin@gmail.com>
2019-02-02 20:36:34 +0300

    DatabaseMetaData added

--- SQLiteJDBCDriverConnection.java
+++ SQLiteJDBCDriverConnection.java
@@ -1,16 +1,19 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.DatabaseMetaData;
  
 public class SQLiteJDBCDriverConnection {
 
     public static void main(String[] args) {
         
         Connection conn = null;
+        DatabaseMetaData meta = null;
         
         try {
             conn = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/db/test.db");
-            System.out.println("Connection to SQLite has been established.");
+            meta = conn.getMetaData();
+            System.out.println("Connection to SQLite has been established. The driver name is " + meta.getDriverName());
             
         } catch (SQLException e) {
             System.out.println(e.getMessage());
